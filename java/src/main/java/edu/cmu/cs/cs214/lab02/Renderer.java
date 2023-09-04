437cd329a06c91d02fbfce55c0840b0ae234ca9f
szlee118 <szlee118@gmail.com>
2023-09-04 17:25:35 -0400

    add java encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -3,12 +3,7 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
-    // public Rectangle rectangle;
     private Shape shape;
-    
-    /*Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
-    }*/
 
     Renderer(Shape shape) {
         this.shape = shape;
